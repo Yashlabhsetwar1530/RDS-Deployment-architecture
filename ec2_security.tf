68270d83f877201b356c49ccfd892aae595a35d9
Yash Labhsetwar <70754364+Yashlabhsetwar1530@users.noreply.github.com>
2023-04-11 18:59:05 +0530

    Create ec2_security.tf

---
+++ ec2_security.tf
@@ -0,0 +1,18 @@
+# Create security group for EC2 instances
+resource "aws_security_group" "sg_for_instances" {
+  name_prefix = "sg_for_instances"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
