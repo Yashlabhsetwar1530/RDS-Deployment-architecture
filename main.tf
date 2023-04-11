0d64493267802bcb18280e7f48b2b45c1f14cd30
Yash Labhsetwar <70754364+Yashlabhsetwar1530@users.noreply.github.com>
2023-04-11 19:31:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Create EC2 instances
 resource "aws_instance" "web" {
-  count             = 2
-  ami               = "ami-0c55b159cbfafe1f0"
-  instance_type     = "t2.micro"
+  count             = var.count
+  ami               = var.ami
+  instance_type     = var.instance_type
   subnet_id         = count.index == 0 ? aws_subnet.public1.id : aws_subnet.public2.id
   vpc_security_group_ids = [aws_security_group.instances_sg.id]
   tags = {
@@ -12,10 +12,10 @@ resource "aws_instance" "web" {
 
 # Create Elasticache cluster
 resource "aws_elasticache_cluster" "el_cluster" {
-  cluster_id               = "example-elasticache-cluster"
-  engine                   = "redis"
-  node_type                = "cache.t2.micro"
-  num_cache_nodes          = 1
+  cluster_id               = var.cluster_id
+  engine                   = var.engine
+  node_type                = var.node_type
+  num_cache_nodes          = var.num_cache_nodes
   subnet_group_name        = aws_elasticache_subnet_group.el_cluster.name
   parameter_group_name     = aws_elasticache_parameter_group.el_cluster.name
 }
